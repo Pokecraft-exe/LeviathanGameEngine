ff187bea67f202ec24ffcf1d315f00e4c970de23
pokecraft-exe <leviathancorp.officiel@gmail.com>
2024-01-26 16:21:35 +0100

    added buttons

--- GameEngine.py
+++ GameEngine.py
@@ -7,7 +7,15 @@ root.geometry("720x500")
 rightframe = tk.Frame(root, bg='red')
 rightframe.pack(side="right", fill='both')
 
+buttonNewProject = tk.Button(rightframe, text="New Project")
+buttonNewProject.pack(side='bottom', fill='x')
+
+buttonNewProject = tk.Button(rightframe, text="Open Project")
+buttonNewProject.pack(side='top')
+
 projectsframe = tk.Frame(root, bg='green')
-projectsframe.pack(side="right", fill='both')
+projectsframe.pack(side="left", fill='both')
+
+
 
 root.mainloop()
