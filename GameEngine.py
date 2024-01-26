106145593ff05f36c8e3cae26b6bdb1df5e4c29c
Julian Lavis--Fabbri <leviathancorp.officiel@gmail.com>
2024-01-26 16:13:01 +0100

    Add files via upload

---
+++ GameEngine.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import os
+root = tk.Tk()
+root.title("Leviathan Game Engine ~ no project loaded")
+root.geometry("720x500")
+
+rightframe = tk.Frame(root, bg='red')
+rightframe.pack(side="right", fill='both')
+
+root.mainloop()
